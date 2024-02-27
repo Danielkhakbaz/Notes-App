e7668908477bd9e925f305d089fcef0e18e20426
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-02-27 15:26:27 +0330

    (seo): Update the metadata

--- app/page.jsx
+++ app/page.jsx
@@ -15,13 +15,13 @@ export const metadata = {
       "a Note-app using LocalStorage API, so your data will never be removed unless you choose to remove it.",
     "twitter:url": "https://danial-notes-app.vercel.app/",
     "twitter:domain": "danial-notes-app.vercel.app",
-    "twitter:image": "https://danieloo.vercel.app/notes-icon.png",
+    "twitter:image": "https://danial-notes-app.vercel.app/notes-icon.png",
     "twitter:card": "summary_large_image",
     "og:title": "Home | Notes App",
     "og:description":
       "a Note-app using LocalStorage API, so your data will never be removed unless you choose to remove it.",
     "og:url": "https://danial-notes-app.vercel.app/",
-    "og:image": "https://danieloo.vercel.app/notes-icon.png",
+    "og:image": "https://danial-notes-app.vercel.app/notes-icon.png",
     "og:type": "website",
   },
 };
